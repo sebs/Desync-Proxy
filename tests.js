3847a99c5f7975fdabceb0d53d087a517b4c00c8
Sebastian Schürmann <sschuermann@nb-sschuermann.(none)>
2010-03-27 11:44:42 +0100

    - tests for cache, stack, config basically complete

--- tests.js
+++ tests.js
@@ -1,5 +1,10 @@
 var assert = require('assert');
 var stack = require('./stack/stack');
+var config = require('./config');
+var cache  = require('./cache/cache');
+/**
+ * Stack Tests 
+ */
 
 var item = {foo:'foo'};
 stack.push('myid', item);
@@ -10,3 +15,24 @@ assert.equal(false, stack.exists('idontknowu'));
 var element = stack.pop();
 assert.equal(false, stack.exists('myid'));
 assert.deepEqual(element, item);
+
+/**
+ * Config Tests
+ */
+assert.ok(typeof config.host != 'undefined');
+assert.ok(typeof config.concurrency != 'undefined');
+assert.ok(typeof config.endpoint != 'undefined');
+assert.ok(typeof config.lifetime != 'undefined');
+
+
+/**
+ * cache Tests
+ */
+cache.add('myid', item);
+var res = cache.exists('myid');
+assert.ok(res);
+var cachedElement = cache.get('myid');
+assert.deepEqual(cachedElement, item);
+var age = cache.getAge('myid');
+assert.ok(isNaN(age) == false);
+assert.ok(cache.exists('myid'));
